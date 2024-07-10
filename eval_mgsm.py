02034790330826f1fd986fef07e3d20e215106b9
yuta-0514 <y.akutsu0514@gmail.com>
2024-07-10 22:37:45 +0900

    Evolutionary Model Merge

---
+++ eval_mgsm.py
@@ -0,0 +1,52 @@
+import re
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import datasets
+
+
+def extract_regex(text):
+    num_list = re.findall(r'\d+', text)
+    if num_list is not None:
+        return num_list
+    else:
+        return None
+
+
+def score_mgsm(pred, answer_number) -> bool:
+    if "." in pred:
+        pred = pred.rstrip("0").rstrip(".")
+    pred = pred.replace(",", "")
+    if str(answer_number) == pred:
+        return 1
+    else:
+        return 0
+
+
+def main():
+    df = datasets.load_dataset("juletxara/mgsm")
+    df = df["test"]
+
+    model_id = "merge_mgsm_train"
+    model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+    cnt = 0
+    for i in range(len(df)):
+        question = df["question"][i]
+        prompt = f"次の数学の問題を解いてください。最終的な答えを出す前に、解答の推論過程を記述してください。答えには整数の答え以外何も追加しないでください。\n問題:{question}\n答え:"
+        inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+        tokens = model.generate(
+            **inputs,
+            max_new_tokens=128,
+            temperature=0.0,
+            top_p=1.0,
+            do_sample=False,
+        )
+        output = tokenizer.decode(tokens[0], skip_special_tokens=True)
+        try:
+            pred = extract_regex(output)[-1]
+            score = score_mgsm(pred, df["answer_number"][i])
+        except IndexError:
+            score = 0
+        cnt += score
+    print(cnt/len(df))        
--- eval_tasks/mgsm_metric.py
+++ eval_tasks/mgsm_metric.py
@@ -8,7 +8,7 @@ def extract_regex(text):
     else:
         return None
 
-def score_mgsm_binary(pred, question, answer_number, answer) -> bool:
+def score_mgsm(pred, question, answer_number, answer) -> bool:
     if "." in pred:
         pred = pred.rstrip("0").rstrip(".")
     pred = pred.replace(",", "")
@@ -17,21 +17,10 @@ def score_mgsm_binary(pred, question, answer_number, answer) -> bool:
     else:
         return 0
 
-def score_mgsm(pred, question, answer_number, answer):
-    pred = pred.replace(".", "")
-    if int(pred) == answer_number:
-        return 1.
-    else:
-        error = abs(int(pred)-answer_number)/answer_number
-        if error > 1:
-            return 0.
-        else:
-            return 1 - error
-
 def process_results(doc, results):
     try:
         pred = extract_regex(results[0])[-1]
-        score = score_mgsm_binary(results[0], doc["question"], doc["answer_number"], doc["answer"])
+        score = score_mgsm(pred, doc["question"], doc["answer_number"], doc["answer"])
     except IndexError:
         score = 0
     return {"acc": score}
